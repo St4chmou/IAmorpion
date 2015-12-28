04d31946612b371d42df32e37df4e0340dae4c4f
Nico <nicolas.sart2@gmail.com>
2015-12-28 19:48:15 +0100

    Dans le menu : a propos et comment jouer complet

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,4 +1,5 @@
 import java.awt.Color; 
+import java.lang.Object;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
@@ -9,6 +10,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class Fenetre extends JFrame {
 	
@@ -55,8 +58,25 @@ public class Fenetre extends JFrame {
 	this.menu2.add(item4);
 	this.menu2.addSeparator();
 	this.menu2.add(item5);
+	
+	final String regles = "Le Tic Tac Toe est un jeu de réflexion où 2 joueurs s'affrontent sur une grille de 3x3 cases.\n\nChaque joueur rempli à tour de rôle une case de la grille avec le symbole qui lui est attribué :\nX ou O. Lorsque c'est à vous de jouer, cliquez à l'endroit de la grille où vous souhaiter placer  \nvotre symbole pour jouer.\n\n Le gagnant est le joueur qui parvient à aligner 3 fois son symbole : horizontalement,\nverticalement ou en diagonale.\n\n";
+	item6.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent arg0) {
+			JOptionPane jop1 = new JOptionPane();
+			jop1.showMessageDialog(null, regles, "Comment jouer ?", JOptionPane.INFORMATION_MESSAGE);
+		}        
+	});
+	
 	this.menu3.add(item6);
-	this.menu3.addSeparator();
+	this.menu3.addSeparator();	
+	
+	final String infos = "Tic Tac Toe\n\nProjet IA 2015 - 2016\nIng2 Info\n\nJulien BAYART\nNicolas SART\n\n";
+	item7.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent arg0) {
+			JOptionPane jop2 = new JOptionPane();
+			jop2.showMessageDialog(null, infos, "A propos", JOptionPane.INFORMATION_MESSAGE);
+		}        
+	});
 	this.menu3.add(item7);
 	
 	// Ajout des éléments dans la barre de menu
